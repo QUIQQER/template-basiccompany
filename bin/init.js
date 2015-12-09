308ba6ee97eb1019dbbf6dbe337625c30d51b6f5
Michael <michael.danielczok@gmail.com>
2015-12-09 13:30:12 +0100

    Bug mit JS Fehler in Navigation behoben.

--- bin/init.js
+++ bin/init.js
@@ -1,25 +1,6 @@
-require.config({
-
-    baseUrl: URL_BIN_DIR + "QUI",
-
-    paths: {
-        "qui"     : URL_OPT_DIR + "bin/qui/qui",
-        "locale"  : URL_VAR_DIR + "locale/bin",
-        "controls": URL_BIN_DIR + "QUI/controls"
-    },
-
-    waitSeconds: 0,
-    catchError : true,
-
-    map: {
-        "*": {
-            "css": URL_OPT_DIR + "bin/qui/qui/lib/css.js"
-        }
-    }
-});
-
 window.addEvent("domready", function () {
     "use strict";
+
 
     // Load QUI
     require(["qui/QUI"], function (QUI) {
@@ -31,64 +12,6 @@ window.addEvent("domready", function () {
     });
 
     /*
-     * Left nav:
-     * open or close the sub nav
-     */
-
-    var ToggleButton = document.getElements(".quiqqer-fa-levels-icon");
-
-    ToggleButton.addEvent("click", function () {
-        var LiLeft = this.getParent().getParent();
-        var NavSubLeft = LiLeft.getElement("div");
-
-        /*
-         * Wenn menü ZU ist:
-         */
-        if (!NavSubLeft.getSize().y.toInt()) {
-
-            NavSubLeft.setStyles({
-                height  : 0,
-                opacity : 0,
-                overflow: "hidden",
-                display : "block"
-            });
-
-            moofx(NavSubLeft).animate({
-                height  : NavSubLeft.getElement("ul").getSize().y.toInt(),
-                opacity : 1
-            }, {
-                duration: 250,
-                callback: function () {
-                    moofx(NavSubLeft).animate({
-                        height: "100%"
-                    });
-                }
-            });
-            this.addClass("fa-nav-levels-rotate");
-            return;
-        }
-
-        /*
-         * Wenn menü AUF ist:
-         */
-        NavSubLeft.setStyle("overflow", "hidden");
-
-        moofx(NavSubLeft).animate({
-            height : 0,
-            opacity: 0
-        }, {
-            duration: 250,
-            callback: function () {
-                moofx(NavSubLeft).animate({
-                    height: 0
-                });
-            }
-        });
-        this.removeClass("fa-nav-levels-rotate");
-    });
-
-
-    /*
      * Mobile nav:
      * open or close the sub nav
      */
@@ -97,8 +20,9 @@ window.addEvent("domready", function () {
     var NavButtonDropDown = document.getElements(".fa-chevron-down");
 
     NavButtonDropDown.addEvent("click", function () {
-        var Li = this.getParent();
+        var Li = this.getParent('li');
         var NavSub = Li.getElement(".page-header-navigation-sub");
+
         if (NavSub.getStyle("height").toInt() < 1) {
             NavSub.addClass("nav-toggle-sub");
             this.addClass("fa-chevron-down-rotate-mobile");
