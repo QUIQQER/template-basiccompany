4cae320c413c1ab091e4d7721680eb5fa1cb4b04
Patrick Müller <p.mueller@pcsg.de>
2020-01-15 11:47:34 +0100

    refactor: removed unnecessary events

--- src/QUI/TemplateBasicCompany/EventHandler.php
+++ src/QUI/TemplateBasicCompany/EventHandler.php
@@ -14,36 +14,4 @@ use QUI;
  */
 class EventHandler
 {
-    /**
-     * Event : on smarty init
-     * @param \Smarty $Smarty - \Smarty
-     */
-    public static function onSmartyInit($Smarty)
-    {
-        if (!isset($Smarty->registered_plugins['function']) ||
-            !isset($Smarty->registered_plugins['function']['fetch'])
-        ) {
-            $Smarty->registerPlugin(
-                "function",
-                "fetch",
-                "\\QUI\\TemplateBasicCompany\\EventHandler::fetch"
-            );
-        }
-    }
-
-    /**
-     * @param $params
-     * @param $Smarty
-     * @return string
-     */
-    public static function fetch($params, $Smarty)
-    {
-        $template = $params['template'];
-        $path     = OPT_DIR.'quiqqer/template-basiccompany/';
-
-        $Engine = QUI::getTemplateManager()->getEngine();
-        $Engine->assign($params);
-
-        return $Engine->fetch($path.$template);
-    }
 }
