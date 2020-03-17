2f8a799467a51810716d1887dd990921afffd745
michael.danielczok <michael@pcsg.de>
2020-03-17 12:27:11 +0100

    feat: Clear cache on project and site save.

--- src/QUI/TemplateBasicCompany/EventHandler.php
+++ src/QUI/TemplateBasicCompany/EventHandler.php
@@ -14,4 +14,31 @@ use QUI;
  */
 class EventHandler
 {
+    /**
+     * Clear system cache on project save
+     *
+     * @return void
+     */
+    public static function onProjectConfigSave()
+    {
+        try {
+            QUI\Cache\Manager::clear('quiqqer/templateBasicCompany');
+        } catch (QUI\Exception $Exception) {
+            QUI\System\Log::writeException($Exception);
+        }
+    }
+
+    /**
+     * Clear system cache on site save
+     *
+     * @return void
+     */
+    public static function onSiteSave()
+    {
+        try {
+            QUI\Cache\Manager::clear('quiqqer/templateBasicCompany');
+        } catch (QUI\Exception $Exception) {
+            QUI\System\Log::writeException($Exception);
+        }
+    }
 }
